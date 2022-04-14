e61e8d9f4ed3538c0cb72fe5deb9ad66872884d2
smithalex17 <66391311+smithalex17@users.noreply.github.com>
2022-04-15 00:37:10 +1200

    Add files via upload

---
+++ src/main/java/comp261/assig2/AStar.java
@@ -0,0 +1,159 @@
+package comp261.assig2;
+
+/*
+Dijkstra search alogrithm
+
+@author: Simon McCallum, Github Copilot
+*/
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.ResourceBundle.Control;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+// dijkstra class for path finding in the graph
+public class AStar {
+ 
+
+    public static ArrayList<Edge> findShortestPathEdges(Graph graph, Stop start, Stop end) {
+
+        // check if the start and end stops are null
+        if (start == null || end == null) {
+            return null;
+        }
+        // used to check how many nodes you visited. Lower is better H.
+        int totalExplored = 0;
+
+        ArrayList<Stop> stops = graph.getStopList(); 
+        
+        // creating a comparison function for the priority queue based on Path
+        Comparator<PathItem> pathStopCompare = new PathCostComparator();
+        // create a new priority queue for the finge        
+        PriorityQueue<PathItem> fringe = new PriorityQueue<PathItem>(pathStopCompare);
+        
+        // create a new array list for the path to be extracted
+        ArrayList<PathItem> visitedStops = new ArrayList<PathItem>();
+       
+        int currentFingeCost = 0;
+
+        // vital step to make sure you can find the new path
+        graph.resetVisited();
+
+        start.setCost(0);
+
+        double f = g(start) + heuristic(start, end); // also could be f(start,start, 0, start,end);
+        fringe.add(new PathItem(start, 0, f, null, null)); // the input is (node, cost, f, prev, edge)
+
+        // while the queue is not empty
+        while (!fringe.isEmpty()) {
+            // TODO: get the stop with the lowest cost
+            // get the top of the queue
+            PathItem current = fringe.poll();
+            Stop currentStop = current.getStop();
+            // TODO: check if the stop from the queue has been visited
+            if(!visitedStops.contains(currentStop)){
+                // TODO: if the stop has not been visited, mark as visited and add to visitedStops list
+                visitedStops.add(current);
+                // TODO: set the cost for the current node
+                totalExplored++;
+
+                // TODO: if the current stop is the end stop
+               if(currentStop.equals(end)) {
+                    // TODO: path to return
+
+                    ArrayList<Edge> shortestEdgePath = makeEdgePath(graph, visitedStops, start, end);
+                    return shortestEdgePath;
+                }
+
+                // TODO: go through each of the current stop's neighbours and add to the fringe
+                for(Edge edge : currentStop.getNeighbours()){
+                    // TODO: get the neighbour from the edge
+                    Stop neighbour = edge.getToStop();
+                    // TODO: if the neighbour has not been visited already
+                    if(!visitedStops.contains(neighbour)){
+                        //TODO: set the neighbour's cost to the current stop's cost + the edge's cost (time or distance)
+                        double cost = currentStop.getCost() + edge.getCost();
+                        neighbour.setCost(cost);
+                        // Error checking is useful
+                        if (cost < 0) {
+                            System.out.println("Error: negative cost");
+                        }
+                        double g = g(neighbour);                                                                   
+                        // TODO: calculate the new f value using g edge cost and heuristic
+                        // something like
+                        f = g + edge.getCost() + heuristic(neighbour, end);                   
+                        // TODO: add the neighbour to the queue as a new PathItem
+                        // using somthing like neighbour, cost, f, prev, edge 
+                        PathItem p = new PathItem(neighbour, cost, f,  edge.getFromStop(), edge);
+                        fringe.add(p);
+                    }
+                }
+            }
+        }
+        // comment on failue
+        System.out.println("Error: " + start + " to " + end + " not found");
+        return new ArrayList<Edge>();
+    }
+
+
+
+    /**
+     * build a path from the end back to the start from the PathItem data
+     * 
+     * @param graph
+     * @param visited the nodes visited while searching for this trip
+     * @param start
+     * @param goal
+     * @return the list of stops in the path
+     */
+    private static ArrayList<Edge> makeEdgePath(Graph graph, ArrayList<PathItem> visited, Stop start, Stop goal) {
+        ArrayList<Edge> path = new ArrayList<Edge>();
+        Edge currentItem = visited.get(visited.size() - 1).getEdge(); // the last edge added is on the path
+
+        // while the current item is not the start
+        while (currentItem.getFromStop() != start) {
+            path.add(currentItem);
+            // find the stop from the visited PathItems
+            for (PathItem visitedItems : visited) {
+                if (visitedItems.getEdge() == null) {
+                    continue;
+                }
+                if (visitedItems.getEdge().getToStop() == currentItem.getFromStop()) {
+                    if (visitedItems.getEdge().getToStop() == null) {
+                        return null;
+                    }
+                    currentItem = visitedItems.getEdge();
+                }
+            }
+            if (currentItem == null) { // if not invalid path
+                System.out.println("error: ");
+                return null;
+            }
+        }
+        // finally add the start and return
+        path.add(currentItem);
+        return path;
+    }
+
+
+    public static double f( Stop current, double edgeCost, Stop neighbour, Stop end) {
+        return g(current) + edgeCost + heuristic(neighbour, end);
+    }
+
+    public static double g( Stop current) {
+        return current.getCost();
+    }
+
+    public static double heuristic(Stop current, Stop goal) {
+        // TODO: calculate a heuristic for the current stop to the goal stop
+        double h = current.distance(goal);
+        return h;
+    }
+
+}
---
+++ src/main/java/comp261/assig2/Graph.java
@@ -0,0 +1,252 @@
+package comp261.assig2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import javafx.scene.paint.Color;
+
+public class Graph {
+
+    private HashMap<String, Stop> stopsMap;
+    private HashMap<String, Trip> tripsMap;
+
+    private ArrayList<Stop> stopList;
+    public Trie trie;
+
+    public Zoning geoJson;
+
+    private int subGraphs = 0;
+
+    public Graph(HashMap<String, Stop> stops, HashMap<String, Trip> trips) {
+        this.stopsMap = stops;
+        this.tripsMap = trips;
+        buildStopList();
+        createNeighbours();
+    }
+
+    // constructor with parsing
+    public Graph(File stopFile, File tripFile, File geoJsonFile) {
+        stopsMap = new HashMap<String, Stop>();
+        tripsMap = new HashMap<String, Trip>();
+        stopsMap = Parser.parseStops(stopFile);
+        tripsMap = Parser.parseTrips(tripFile);
+        geoJson = Parser.parseGeoJson(geoJsonFile);
+        // caclculate exact memory usage of the graph
+
+        buildStopList();
+        attachTripsToStops();
+        createNeighbours();
+
+    }
+
+    // buildStoplist from hashmap
+    private void buildStopList() {
+        stopList = new ArrayList<Stop>();
+        for (Stop s : stopsMap.values()) {
+            stopList.add(s);
+        }
+    }
+
+
+
+    // attach trip data to each stop
+    private void attachTripsToStops() {
+        for (Trip trip : tripsMap.values()) {
+            for (String stopId : trip.getStopIds()) {
+                Stop stop = stopsMap.get(stopId);
+                if (stop != null) {
+                    // add the trip to the stop
+                    stop.addTrip(trip);
+                } else {
+                    System.out.println("Missing stop pattern id: " + stopId);
+                }
+            }
+        }
+    }
+
+    /**
+     * For every stop tell it to construct the edges associated with the trips that have been stored in it.
+     */
+    private void createNeighbours() {
+        for (Stop stop : stopsMap.values()) {
+            stop.makeNeighbours(this.stopsMap);
+        }
+    }
+
+    // get first stop that starts with a search string
+    public Stop getFirstStop(String search) {
+        // Search for the first stop matching the search string
+        // This is slow and would be faster with a Trie 
+        Stop firstStop = null;
+        for (Stop stop : stopList) {
+            if (stop.getName().startsWith(search)) {
+                firstStop = stop;
+                break;
+            }
+        }
+        return firstStop;
+        // This would be the call to the Trie from Assignment 1
+        //return trie.getAll(search).get(0);
+    }
+
+    // get all stops that start with a search string
+    public List<Stop> getAllStops(String search) {
+        //search for all stops matching the search string
+        List<Stop> allStops = new ArrayList<Stop>();
+        for (Stop stop : stopList) {
+            if (stop.getName().startsWith(search)) {
+                allStops.add(stop);
+            }
+        }
+        return allStops;
+        // This would be the call to the Trie from Assignment 1
+        // return trie.getAll(search);
+    }
+
+    // getter for stopList
+    public ArrayList<Stop> getStopList() {
+        return stopList;
+    }
+
+    public HashMap<String, Stop> getStops() {
+        return stopsMap;
+    }
+
+    public void setStops(HashMap<String, Stop> stops) {
+        this.stopsMap = stops;
+    }
+
+    public HashMap<String, Trip> getTrips() {
+        return tripsMap;
+    }
+
+    public void setTrips(HashMap<String, Trip> trips) {
+        this.tripsMap = trips;
+    }
+
+
+
+    public void resetVisited() {
+        for (Stop stop : stopList) {
+            stop.setVisited(false);
+            stop.setCost(Double.MAX_VALUE);
+        }
+    }
+
+    // I have used Kosaraju's_algorithm from https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
+    // You can use this or use Tarjan's algorithm for strongly connected components https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
+    // find graph components and lable the nodes in each component
+    public int findComponents() {
+        // TODO: implement component analysis
+        // TODO: reset visited and cost
+        for(Stop s : stopList){
+            s.setVisited(false);
+            s.setCost(0);
+        }
+        // TODO: go through all stops use recursive to visitAllConnections and build visit order
+        ArrayList<Stop> visitOrder = new ArrayList<Stop>();
+        for(Stop s : stopList){
+            visitAllConnections(s, visitOrder);
+        }
+        // TODO: search in reverse visit order setting the root node recurssively on assignRoot
+         // TODO: keep a count of the components
+        int components = 0;
+        for(int i = visitOrder.size()-1; i <= 0; i--){
+            assignRoot(visitOrder.get(i), visitOrder.get(i+1), components);
+        }       
+        // TODO: set the subgraphs to number of components and return it
+        subGraphs = components;
+        return subGraphs;
+    }
+
+    /**
+     * Recursively visit all connections of a stop and build a list of stops visited from this stop
+     */
+    private void visitAllConnections(Stop stop, ArrayList<Stop> visitOrder) {
+        stop.setVisited(true);
+        visitOrder.add(stop);
+        // TODO: set vistited to true
+        // TODO: add stop to visitOrder
+        // TODO: for each edge of the stop if the ToStop is not visited, recurse this function with the toStop of the neighbour Edge
+        for(Edge e : stop.getNeighbours()){
+            if(!e.getToStop().isVisited()){
+                visitAllConnections(e.getToStop(), visitOrder);
+            }
+        }
+    }
+
+    /**
+     * Recursively assign the root node of a subgraph
+     */
+    public void assignRoot(Stop stop, Stop root, int component) {
+        // TODO: set the root of the subgraph to the stop, and the subgraph ID
+        // TODO: for each of the edges in neighbours if the toStop root is empty recurse assigning root and component
+        stop.setRoot(root);
+        for(Edge e  : stop.getNeighbours()){
+            if(e.getToStop().getRoot() == null){
+                assignRoot(e.getToStop(), stop, component);
+            }
+        }
+        
+    }
+
+    /**
+     * reset the root and the subgraph ID of all stops
+     */
+    public void resetRoot() {
+        for (Stop stop : stopList) {
+            stop.setRoot(null);
+            stop.setSubGraphId(-1);
+        }
+    }
+
+
+    public int getSubGraphCount() {
+        return subGraphs;
+    }
+
+    // add walking edges
+    public void addWalkingEdges(double walkingDistance) {
+        // TODO: add walking edges to all stops
+        int count = 0;
+        // TODO: step through all stops and all potential neighbours 
+        for(Stop stopFrom : stopList){
+            for(Stop stopTo : stopList){
+                if(stopFrom.getId() != stopTo.getId()){
+                    //TODO: check the distannce between to stops and if it is less then walkingDistance add an edge to the graph
+                    // something like:   
+                    // stopFrom.addNeighbour( new Edge(stopFrom, stopTo, Transport.WALKING_TRIP_ID, stopFrom.distance(stopTo)/Transport.WALKING_SPEED_MPS));
+                    // count the number of edges added 
+                    double dis = stopFrom.distance(stopTo);
+                    if(dis < walkingDistance){
+                        stopFrom.addNeighbour( new Edge(stopFrom, stopTo, Transport.WALKING_TRIP_ID, stopFrom.distance(stopTo)/Transport.WALKING_SPEED_MPS));
+                        count++;
+                    }
+                }
+                    
+            }
+        }
+        System.out.println("Walking edges added: " + count);
+    }
+
+    // remove walking edges  - could just make them invalid or check the walking_checkbox
+    public void removeWalkingEdges() {
+        for (Stop stop : stopList) {
+            stop.deleteAllEdges(Transport.WALKING_TRIP_ID);// remove all edges with the walking trip id
+        }
+    }
+
+    // A nicer way to build a string of a list of stops
+    public String DisplayStops(List<Stop> listOfStops) {
+        String returnString = "";
+        for (Stop stop : listOfStops) {
+            returnString += stop.getName() + "\n";
+        }
+        return returnString;
+    }
+
+}
